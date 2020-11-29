4ba0bf2f4b7cebcfb020ffb510e12357c1c81162
Laurence Midgley <loll.midgley@gmail.com>
2020-11-29 13:33:20 +0000

    Update Nice_scientific_notation_without_logscale.py

--- Python/Matplotlib/Nice_scientific_notation_without_logscale.py
+++ Python/Matplotlib/Nice_scientific_notation_without_logscale.py
@@ -1,4 +1,4 @@
 # Nice scientific notation without having to use log_scale
 f = mpl.ticker.ScalarFormatter(useOffset=False, useMathText=True)
 g = lambda x,pos : "${}$".format(f._formatSciNotation('%1.10e' % x))
-plt.gca().yaxis.set_major_formatter(mpl.ticker.FuncFormatter(g))
+plt.gca().yaxis.set_major_formatter(mpl.ticker.FuncFormatter(g)) # if there is a specific axis (e.g. when using subplots, then call on specific axis (ax[0].yaxis...etc) instead of gca
