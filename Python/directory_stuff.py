8eeac1067a9d6cb45007109026062d74fb2c80d5
Laurence Midgley <loll.midgley@gmail.com>
2021-02-06 13:27:57 +0200

    Update and rename os_stuff.py to directory_stuff.py

--- Python/directory_stuff.py
+++ Python/directory_stuff.py
@@ -1,3 +1,11 @@
 # How to make a new paths if intermediate folders are nested
 if not os.path.exists(os.path.join(os.getcwd(), target_path)): # check if path exists, also shows how to use os join
     os.makedirs(os.path.join(os.getcwd(), target_path))
+    
+    
+ # set path to parent file
+from pathlib import Path
+import os
+cwd_path = Path.cwd()
+set_path = str(cwd_path.parent)
+os.chdir(set_path)
