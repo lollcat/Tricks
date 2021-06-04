5abb3b2c0ec9ef221cd0cb90da2506706e23d92d
Kristopher Miltiadou <64787863+Bottledd@users.noreply.github.com>
2021-06-04 11:07:51 +0100

    Update Reload_module.py

--- Python/Jupyter/Reload_module.py
+++ Python/Jupyter/Reload_module.py
@@ -2,3 +2,8 @@ import importlib
 import LDA_class # from python file that get's updated (in which case we need to import the updated version)
 importlib.reload(LDA_class)
 LDA = LDA_class.LDA   # equivalent to reloading from LDA_class import LDA
+
+
+# BETTER WAY
+%load_ext autoreload
+%autoreload 2
